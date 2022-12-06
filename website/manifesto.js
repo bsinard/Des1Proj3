354727937c597a55e36659a9e1b00cc62579c619
bsinard <117944082+bsinard@users.noreply.github.com>
2022-12-06 13:12:51 -0600

    Finished first iteration of website

---
+++ website/manifesto.js
@@ -0,0 +1,10 @@
+//const eyeball = document.querySelector(".ball");
+
+//document.onmousemove = () => {
+  //const x = event.clientX * 100 / window.innerWidth + "%";
+  //const y = event.clientY * 100 / window.innerHeight + "%";
+
+//  eyeball.sytle.left = x;
+//  eyeball.style.top = y;
+//  eye.style.transform = "translate(-"+x+", -"+y+")"
+//}
